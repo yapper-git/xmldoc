cd06a5c2548c43aaf906169a53d69e697ed54ee1
yvand <yvan.duron@gmail.com>
2014-11-28 21:32:12 +0100

    Update xml2html.py (argparse)

--- xml2html.py
+++ xml2html.py
@@ -1,12 +1,34 @@
 #!/usr/bin/env python
 
+import argparse
+import os
 import sys
 import xml.etree.ElementTree as ET
+
 from DocParser import *
 from XHTMLRenderer import *
 
-root = ET.parse(sys.argv[1]).getroot()
+def error(message):
+    print("ERROR:", message, file=sys.stderr)
+
+parser = argparse.ArgumentParser()
+parser.add_argument("--force", "-f", help="force output overwrite", action="store_true")
+parser.add_argument("input", help="path to input file")
+parser.add_argument("output", help="path to output file")
+args = parser.parse_args()
+
+# Check input file existance
+if not os.path.isfile(args.input):
+    error("{} does not exist".format(args.input))
+    sys.exit(1)
+
+# Check output file existance (do not overwrite by default)
+if not args.force and os.path.isfile(args.output):
+    error("{} already exists, use -f to force overwrite".format(args.output))
+    sys.exit(2)
+
+root = ET.parse(args.input).getroot()
 parser = DocParser(root, XHTMLRenderer())
 
-with open(sys.argv[2], 'w') as file:
+with open(args.output, "w") as file:
     file.write(parser.run())
