87856411fcb03841cdfef720e54c1a278528c07d
yvand <yvan.duron@gmail.com>
2014-11-29 22:51:57 +0100

    Improve extrat_to_xhtml to make a valid XHTML file (with DOCTYPE, <head> and <body>)

--- extract_to_xhtml.py
+++ extract_to_xhtml.py
@@ -8,11 +8,19 @@ import xml.etree.ElementTree as ET
 from DocParser import *
 from XHTMLRenderer import *
 
+DEFAULT_LANG = "en"
+DEFAULT_TITLE = "Untitled extract"
+
 parser = argparse.ArgumentParser(description="Converts XML extract to XHTML.")
 parser.add_argument("-f", "--force", help="force output overwrite", action="store_true")
+parser.add_argument("-l", "--lang", help="extract language ('{}' by default)".format(DEFAULT_LANG))
+parser.add_argument("-t", "--title", help="extract title ('{}' by default)".format(DEFAULT_TITLE))
 parser.add_argument("input", help="path to input file")
 parser.add_argument("output", help="path to output file")
 args = parser.parse_args()
+
+lang = args.lang if args.lang else DEFAULT_LANG
+title = args.title if args.title else DEFAULT_TITLE
 
 try:
     if not args.force and os.path.isfile(args.output):
@@ -22,8 +30,26 @@ try:
     
     parser = DocParser(root, XHTMLRenderer())
     
+    content = (
+        '<!DOCTYPE html PUBLIC "{dtd_name}" "{dtd_location}">\n'
+        '<html xmlns="http://www.w3.org/1999/xhtml" xml:lang="{lang}" lang="{lang}" dir="ltr">\n'
+        '  <head>\n'
+        '    <meta http-equiv="Content-Type" content="text/html; charset=utf-8" />\n'
+        '    <title>{title}</title>\n'
+        '  </head>\n'
+        '  <body>\n'
+        '    <h1>{title}</h1><hr />\n'
+        '    {content}\n'
+        '  </body>\n'
+        '</html>'.format(
+            dtd_name="-//W3C//DTD XHTML 1.0 Strict//EN",
+            dtd_location="http://www.w3.org/TR/xhtml1/DTD/xhtml1-strict.dtd",
+            lang=lang,
+            title=title,
+            content=parser.run()))
+    
     with open(args.output, "w") as file:
-        file.write(parser.run())
+        file.write(content)
 except Exception as exception:
     print(exception, file=sys.stderr)
     sys.exit(1)
