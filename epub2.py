01c0f9c93051e330a1e93da496092863d67977fc
yvand <yvan.duron@gmail.com>
2014-11-29 14:45:56 +0100

    Add ePUB2 builder (epub2.py) and examples in tests/epub

---
+++ epub2.py
@@ -0,0 +1,443 @@
+import zipfile
+
+class Metadata:
+    """Metadata represents the metadata element in .opf file"""
+    
+    def __init__(self):
+        self._titles       = []
+        self._creators     = []
+        self._subjects     = []
+        self._description  = None
+        self._publisher    = None
+        self._contributors = []
+        self._dates        = []
+        self._type         = None
+        self._format       = None
+        self._identifiers  = []
+        self._source       = None
+        self._languages    = []
+        self._relation     = None
+        self._coverage     = None
+        self._rights       = None
+        self._metas        = []
+    
+    def addTitle(self, title):
+        self._titles.append(title)
+    
+    def addCreator(self, name, role=None, fileAs=None):
+        self._creators.append({
+            "name": name,
+            "role": role,
+            "fileAs": fileAs
+        })
+    
+    def addSubject(self, subject):
+        self._subjects.append(subject)
+    
+    def setDescription(self, description):
+        self._description = description
+    
+    def setPublisher(self, publisher):
+        self._publisher = publisher
+    
+    def addContributor(self, name, role=None, fileAs=None):
+        self._contributors.append({
+            "name": name,
+            "role": role,
+            "fileAs": fileAs
+        })
+    
+    def addDate(self, date, event=None):
+        self._dates.append({"date": date, "event": event})
+    
+    def setType(self, type):
+        self._type = type
+    
+    def setFormat(self, format):
+        self._format = format
+    
+    def addIdentifier(self, content, id=None, scheme=None):
+        self._identifiers.append({
+            "content": content,
+            "id": id,
+            "scheme": scheme
+        })
+    
+    def setSource(self, source):
+        self._source = source
+    
+    def addLanguage(self, language):
+        self._languages.append(language)
+    
+    def setRelation(self, relation):
+        self._relation = relation
+    
+    def setCoverage(self, coverage):
+        self._coverage = coverage
+    
+    def setRights(self, rights):
+        self._rights = rights
+    
+    def addMeta(self, name, content):
+        self._metas.append({"name": name, "content": content})
+
+
+class Manifest:
+    """Manifest represents the manifest element in .opf file"""
+    
+    def __init__(self):
+        self._items = []
+    
+    def __iter__(self):
+        return iter(self._items)
+    
+    def addItem(self, id, href, mediaType):
+        self._items.append({
+            "id": id,
+            "href": href,
+            "mediaType": mediaType
+        })
+
+
+class Spine:
+    """Spine represents the spine element in .opf file"""
+    
+    def __init__(self):
+        self._itemrefs = []
+    
+    def __iter__(self):
+        return iter(self._itemrefs)
+    
+    def addItemref(self, idref, linear=True):
+        self._itemrefs.append({"idref": idref, "linear": linear})
+
+
+class Guide:
+    """Guide represents the guide element in .opf file"""
+    
+    def __init__(self):
+        self._references = []
+    
+    def __iter__(self):
+        return iter(self._references)
+    
+    def addReference(self, type, title, href):
+        self._references.append({
+            "type": type,
+            "title": title,
+            "href": href
+        })
+
+
+class Contents:
+    
+    def __init__(self):
+        self._navPoints = []
+    
+    def addNavPoint(self, navPoint):
+        self._navPoints.append(navPoint)
+
+
+class NavPoint:
+    """NavPoint represents a NavPoint element for contents"""
+    
+    #_playOrder = 0
+    
+    def __init__(self, id, label, source, navPoints=[]):
+        self._id = id
+        self._label = label
+        self._source = source
+        self._navPoints = navPoints
+        #self._playOrder = NavPoint.
+    
+    def append(navPoint):
+        self._navPoints.append(navPoint)
+
+
+class Epub:
+    
+    encoding = "UTF-8"
+    tab = "\t"
+    end = "\n"
+    
+    def __init__(self, filePath):
+        self.identifier = None
+        self.title = None
+        self.language = None
+        self.metadata = Metadata()
+        self.manifest = Manifest()
+        self.spine = Spine()
+        self.guide = Guide()
+        self.contents = Contents()
+        
+        self._zip = zipfile.ZipFile(filePath, "w")
+        self._writeMimetypeFile()
+        self._writeContainerFile()
+        self.manifest.addItem("ncx", "toc.ncx", "application/x-dtbncx+xml")
+    
+    def close(self):
+        if not self.identifier:
+            raise ValueError("identifier required")
+        if not self.title:
+            raise ValueError("title required")
+        if not self.title:
+            raise ValueError("language required")
+        if len(self.contents._navPoints) == 0:
+            raise ValueError("navPoint required")
+        self._writeOpfFile()
+        self._writeNcxFile()
+        self._zip.close()
+    
+    def addFileFromFile(self, localname, filename, id, mediaType):
+        self._zip.write(filename, "OEBPS/" + localname)
+        self.manifest.addItem(id, localname, mediaType)
+    
+    def addFileFromString(self, localname, content, id, mediaType):
+        self._zip.writestr("OEBPS/" + localname, content)
+        self.manifest.addItem(id, localname, mediaType)
+    
+    def addTextFromFile(self, localname, filename, id):
+        self.addFileFromFile(localname, filename, id, "application/xhtml+xml")
+        self.spine.addItemref(id)
+    
+    def addTextFromString(self, localname, content, id):
+        self.addFileFromString(localname, content, id, "application/xhtml+xml")
+        self.spine.addItemref(id)
+    
+    def addStyleFromFile(self, localname, filename, id):
+        self.addFileFromFile(localname, filename, id, "text/css")
+    
+    def addStyleFromString(self, localname, content, id):
+        self.addFileFromString(localname, content, id, "text/css")
+    
+    def _writeMimetypeFile(self):
+        self._zip.writestr("mimetype", "application/epub+zip")
+    
+    def _writeContainerFile(self):
+        self._zip.writestr("META-INF/container.xml",
+            '<?xml version="1.0" encoding="{encoding}"?>{end}'
+            '<container version="1.0" xmlns="{xmlns}">{end}'
+            '{tab}<rootfiles>{end}'
+            '{tab}{tab}<rootfile full-path="OEBPS/content.opf" media-type="application/oebps-package+xml"/>{end}'
+            '{tab}</rootfiles>{end}'
+            '</container>'.format(
+                xmlns="urn:oasis:names:tc:opendocument:xmlns:container",
+                encoding=Epub.encoding, tab=Epub.tab, end=Epub.end))
+    
+    def _writeOpfFile(self):
+        self._zip.writestr("OEBPS/content.opf",
+            '<?xml version="1.0" encoding="{encoding}"?>{end}'
+            '<package xmlns="{xmlns_opf}" version="2.0" unique-identifier="BookId">{end}'
+            '{tab}<metadata xmlns:dc="{xmlns_dc}" xmlns:opf="{xmlns_opf}">{end}'
+            '{tab}{tab}{metadata}{end}'
+            '{tab}</metadata>{end}'
+            '{tab}<manifest>{end}'
+            '{tab}{tab}{manifest}{end}'
+            '{tab}</manifest>{end}'
+            '{tab}<spine toc="ncx">{end}'
+            '{tab}{tab}{spine}{end}'
+            '{tab}</spine>{end}'
+            '</package>'.format(
+                xmlns_opf="http://www.idpf.org/2007/opf",
+                xmlns_dc="http://purl.org/dc/elements/1.1/",
+                encoding=Epub.encoding,
+                tab=Epub.tab,
+                end=Epub.end,
+                metadata="\n\t\t".join(self._metadataTags()),
+                manifest="\n\t\t".join(self._manifestTags()),
+                spine="\n\t\t".join(self._spineTags())
+            ))
+    
+    def _writeNcxFile(self):
+        self._zip.writestr("OEBPS/toc.ncx",
+            '<?xml version="1.0" encoding="{encoding}"?>{end}'
+            '<!DOCTYPE ncx PUBLIC "{dtd_name}" "{dtd_location}">{end}'
+            '<ncx version="2005-1" xmlns="http://www.daisy.org/z3986/2005/ncx/">{end}'
+            '{tab}<head>{end}'
+            '{tab}{tab}<meta name="dtb:uid" content="{identifier}"/>{end}'
+            '{tab}{tab}<meta name="dtb:depth" content="{depth}"/>{end}'
+            '{tab}{tab}<meta name="dtb:totalPageCount" content="0"/>{end}'
+            '{tab}<meta name="dtb:maxPageNumber" content="0"/>{end}'
+            '{tab}</head>{end}'
+            '{tab}<docTitle><text>SIC</text></docTitle>'
+            '<navMap>{end}'
+            '{tab}{tab}{navmap}{end}'
+            '{tab}</navMap>{end}'
+            '</ncx>'.format(
+                dtd_name="-//NISO//DTD ncx 2005-1//EN",
+                dtd_location="http://www.daisy.org/z3986/2005/ncx-2005-1.dtd",
+                encoding=Epub.encoding,
+                tab=Epub.tab,
+                end=Epub.end,
+                identifier=self.identifier,
+                depth=self.depth(),
+                navmap=self.renderNavMap()
+            ))
+    
+    def _manifestTags(self):
+        tags = []
+        for item in self.manifest:
+            tags.append('<item id="{}" href="{}" media-type="{}"/>'
+                        .format(item["id"], item["href"], item["mediaType"]))
+        return tags
+    
+    def _spineTags(self):
+        tags = []
+        for itemref in self.spine:# FIXME ugly
+            tag = '<itemref idref="{}"'.format(itemref["idref"])
+            if not itemref["linear"]: tag += ' linear="no"'
+            tag += '/>'
+            tags.append(tag)
+        return tags
+    
+    def _guideTags(self):
+        tags = []
+        for type, title, href in self.guide:
+            tags.append('<reference type="{}" title="{}" href="{}"/>'
+                        .format(type, title, href))
+        return tags
+
+    def _metadataTags(self):
+        metadata = self.metadata
+        tags = []
+
+        # titles
+        tags.append('<dc:title>{}</dc:title>'.format(self.title))
+        for title in metadata._titles:
+            tags.append('<dc:title>{}</dc:title>'.format(title))
+
+        # creators
+        for creator in metadata._creators:
+            tag = '<dc:creator'
+            if creator["role"]: tag += ' opf:role="{}"'.format(creator["role"])
+            if creator["fileAs"]: tag += ' opf:file-as="{}"'.format(creator["fileAs"])
+            tag += '>{}</dc:creator>'.format(creator["name"])
+            tags.append(tag)
+
+        # subjects
+        for subject in metadata._subjects:
+            tags.append('<dc:subject>{}</dc:subject>'.format(subject))
+
+        # description
+        if metadata._description:
+            tags.append('<dc:description>{}</dc:description>'.format(metadata._description))
+
+        # publisher
+        if metadata._publisher:
+            tags.append('<dc:publisher>{}</dc:publisher>'.format(metadata._publisher))
+
+        # contributors
+        for contributor in metadata._contributors:
+            tag = '<dc:contributor'
+            if contributor["role"]: tag += ' opf:role="{}"'.format(contributor["role"])
+            if contributor["fileAs"]: tag += ' opf:file-as="{}"'.format(contributor["fileAs"])
+            tag += '>{}</dc:contributor>'.format(contributor["name"])
+            tags.append(tag)
+
+        # dates
+        for date in metadata._dates:
+            tag = '<dc:date'
+            if date["event"]: tag += ' opf:event="{}"'.format(date["event"])
+            tag += '>{}</dc:date>'.format(date["date"])
+            tags.append(tag)
+
+        # type
+        if metadata._type:
+            tags.append('<dc:type>{}</dc:type>'.format(metadata._type))
+
+        # format
+        if metadata._format:
+            tags.append('<dc:format>{}</dc:format>'.format(metadata._format))
+
+        # identifiers
+        tags.append('<dc:identifier id="BookId">{}</dc:identifier>'.format(self.identifier))
+        for identifier in metadata._identifiers:
+            tag = '<dc:identifier'
+            if identifier["id"]: tag += ' id="{}"'.format(identifier["id"])
+            if identifier["scheme"]: tag += ' opf:scheme="{}"'.format(identifier["scheme"])
+            tag += '>{}</dc:identifier>'.format(identifier["content"])
+            tags.append(tag)
+
+        # source
+        if metadata._source:
+            tags.append('<dc:source>{}</dc:source>'.format(metadata._source))
+
+        # languages
+        tags.append('<dc:language>{}</dc:language>'.format(self.language))
+        for language in metadata._languages:
+            tags.append('<dc:language>{}</dc:language>'.format(language))
+
+        # relation
+        if metadata._relation:
+            tags.append('<dc:relation>{}</dc:relation>'.format(metadata._relation))
+
+        # coverage
+        if metadata._coverage:
+            tags.append('<dc:coverage>{}</dc:coverage>'.format(metadata._coverage))
+
+        # rights
+        if metadata._rights:
+            tags.append('<dc:rights>{}</dc:rights>'.format(metadata._rights))
+
+        # metas
+        for name, content in metadata._metas:
+            tags.append('<meta name="{}" content="{}"/>'.format(name, content))
+        
+        return tags
+    
+    def renderNavMap(self):
+        buf = ""
+        for navPoint in self.contents._navPoints:
+            buf += self.renderNavPoint(navPoint, 2)
+        return buf
+    
+    def renderNavPoint(self, navPoint, level):
+        # FIXME ugly
+        # TODO implement playOrder
+        
+        tab = Epub.tab
+        end = Epub.end
+        
+        buffer = ""
+        
+        # <navPoint id="..." playOrder="...">
+        buffer += (tab * level) + '<navPoint id="{}">'.format(navPoint._id) + end
+        
+        # <navLabel><text>...</text></navLabel>
+        buffer += (tab * (level+1)) + '<navLabel>' + end
+        buffer += (tab * (level+2)) + '<text>' + navPoint._label + '</text>' + end
+        buffer += (tab * (level+1)) + '</navLabel>' + end
+        
+        # <content src="..."/>
+        buffer += (tab * (level+1)) + '<content src="' + navPoint._source + '"/>' + end
+        
+        # <navPoint>...</navPoint> if children
+        for childNavPoint in navPoint._navPoints:
+            buffer += self.renderNavPoint(childNavPoint, level+1)
+        
+        # </navPoint>
+        buffer += (tab * level) + '</navPoint>' + end
+        
+        return buffer
+
+
+    def depth(self):
+        maxDepth = 0;
+        for navPoint in self.contents._navPoints:
+            depth = self.depthRec(navPoint, 1)
+            if depth > maxDepth:
+                maxDepth = depth
+        return maxDepth;
+    
+    def depthRec(self, navPoint, currentDepth):
+        navPoints = navPoint._navPoints
+        
+        if len(navPoints) == 0:
+            return currentDepth
+        else:
+            max = currentDepth;
+            for childNavPoint in navPoints:
+                depth = self.depthRec(childNavPoint, currentDepth+1)
+                if depth > max:
+                    max = depth;
+            return max;
---
+++ tests/epub/sample.py
@@ -0,0 +1,109 @@
+import epub2
+
+epub = epub2.Epub("sample.epub")
+
+epub.identifier = "sample"
+epub.title = "Sample ePUB file"
+epub.language = "en-GB"
+
+epub.metadata.addTitle("Title N°2")
+epub.metadata.addTitle("Title N°3")
+epub.metadata.addCreator("Creator N°1")
+epub.metadata.addCreator("Creator N°2", role="aut")
+epub.metadata.addCreator("Creator N°3", fileAs="Creator 3", role="oth")
+epub.metadata.addSubject("Subject N°1")
+epub.metadata.addSubject("Subject N°2")
+epub.metadata.setDescription("Description")
+epub.metadata.addContributor("Contributor N°1")
+epub.metadata.addContributor("Contributor N°2", role="aut")
+epub.metadata.addContributor("Contributor N°3", fileAs="Contributor 3", role="oth")
+epub.metadata.addDate("2014")
+epub.metadata.addDate("2014-12", event="modification")
+epub.metadata.addDate("2014-12-25", event="publication")
+epub.metadata.setType("type")
+epub.metadata.setFormat("format")
+epub.metadata.addIdentifier("uuid002")
+epub.metadata.addIdentifier("uuid003", "myid", "scheme")
+epub.metadata.setSource("source")
+epub.metadata.addLanguage("en-US")
+epub.metadata.addLanguage("fr-fr")
+epub.metadata.addLanguage("de")
+epub.metadata.setRelation("relation")
+epub.metadata.setCoverage("coverage")
+epub.metadata.setRights("rights")
+epub.metadata.addMeta("name1", "content1")
+epub.metadata.addMeta("name2", "content2")
+
+epub.guide.addReference(type="title-page", title="Title page", href="titlepage.xhtml")
+epub.guide.addReference(type="toc", title="Table of Contents", href="contents.xhtml")
+
+epub.contents.addNavPoint(epub2.NavPoint("chap1", "Chapter 1", "chapter1.xhtml"))
+epub.contents.addNavPoint(epub2.NavPoint("chap2", "Chapter 2", "chapter2.xhtml"))
+
+epub.addTextFromString(id="titlepage", localname="titlepage.xhtml", content="""<?xml version="1.0" encoding="UTF-8"?>
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.1//EN" "http://www.w3.org/TR/xhtml11/DTD/xhtml11.dtd">
+<html xmlns="http://www.w3.org/1999/xhtml" xml:lang="en">
+  <head>
+    <meta http-equiv="Content-Type" content="application/xhtml+xml; charset=utf-8" />
+    <title>Sample ePUB file</title>
+  </head>
+  <body>
+    <h1>Sample ePUB file</h1>
+  </body>
+</html>""")
+
+epub.addTextFromString(id="contents", localname="contents.xhtml", content="""<?xml version="1.0" encoding="UTF-8"?>
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.1//EN" "http://www.w3.org/TR/xhtml11/DTD/xhtml11.dtd">
+<html xmlns="http://www.w3.org/1999/xhtml" xml:lang="en">
+  <head>
+    <meta http-equiv="Content-Type" content="application/xhtml+xml; charset=utf-8" />
+    <title>Tables of Contents</title>
+    <link rel="stylesheet" href="main.css" type="text/css" />
+  </head>
+  <body>
+    <h1>Table of Contents</h1>
+    <ol>
+      <li><a href="chapter1.xhtml">Chapter 1</a></li>
+      <li><a href="chapter2.xhtml">Chapter 2</a></li>
+    </ol>
+  </body>
+</html>""")
+
+epub.addTextFromString(id="chap1", localname="chapter1.xhtml", content="""<?xml version="1.0" encoding="UTF-8"?>
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.1//EN" "http://www.w3.org/TR/xhtml11/DTD/xhtml11.dtd">
+<html xmlns="http://www.w3.org/1999/xhtml" xml:lang="en">
+  <head>
+    <meta http-equiv="Content-Type" content="application/xhtml+xml; charset=utf-8" />
+    <title>Chapiter 1</title>
+    <link rel="stylesheet" href="main.css" type="text/css" />
+  </head>
+  <body>
+    <h1>Chapter 1</h1>
+    <p>Et eodem impetu Domitianum praecipitem per scalas itidem funibus constrinxerunt, eosque coniunctos per ampla spatia civitatis acri raptavere discursu. iamque artuum et membrorum divulsa conpage superscandentes corpora mortuorum ad ultimam truncata deformitatem velut exsaturati mox abiecerunt in flumen.</p>
+    <p>Ideoque fertur neminem aliquando ob haec vel similia poenae addictum oblato de more elogio revocari iussisse, quod inexorabiles quoque principes factitarunt. et exitiale hoc vitium, quod in aliis non numquam intepescit, in illo aetatis progressu effervescebat, obstinatum eius propositum accendente adulatorum cohorte.</p>
+    <p>Eo adducta re per Isauriam, rege Persarum bellis finitimis inligato repellenteque a conlimitiis suis ferocissimas gentes, quae mente quadam versabili hostiliter eum saepe incessunt et in nos arma moventem aliquotiens iuvant, Nohodares quidam nomine e numero optimatum, incursare Mesopotamiam quotiens copia dederit ordinatus, explorabat nostra sollicite, si repperisset usquam locum vi subita perrupturus.</p>
+  </body>
+</html>""")
+
+epub.addTextFromString(id="chap2", localname="chapter2.xhtml", content="""<?xml version="1.0" encoding="UTF-8"?>
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.1//EN" "http://www.w3.org/TR/xhtml11/DTD/xhtml11.dtd">
+<html xmlns="http://www.w3.org/1999/xhtml" xml:lang="en">
+  <head>
+    <meta http-equiv="Content-Type" content="application/xhtml+xml; charset=utf-8" />
+    <title>Chapter 2</title>
+    <link rel="stylesheet" href="main.css" type="text/css" />
+  </head>
+  <body>
+    <h1>Chapter 2</h1>
+    <p>Quam ob rem cave Catoni anteponas ne istum quidem ipsum, quem Apollo, ut ais, sapientissimum iudicavit; huius enim facta, illius dicta laudantur. De me autem, ut iam cum utroque vestrum loquar, sic habetote.</p>
+    <p>Quanta autem vis amicitiae sit, ex hoc intellegi maxime potest, quod ex infinita societate generis humani, quam conciliavit ipsa natura, ita contracta res est et adducta in angustum ut omnis caritas aut inter duos aut inter paucos iungeretur.</p>
+  </body>
+</html>""")
+
+epub.addStyleFromString("main.css", """h1 {
+    border-bottom: 1px solid black;
+}""", "style")
+
+epub.addFileFromFile("tux.png", "face.png", "tux", "image/png")
+
+epub.close()
