d1f4fb477cbd40cc72263ebc188db2961f7d508a
yvand <yvan.duron@gmail.com>
2015-08-15 17:51:49 +0200

    Modifications dans minifier.py

--- minifier.py
+++ minifier.py
@@ -6,6 +6,10 @@ class Minifier:
     INLINE_ELEMENTS = ["h1", "h2", "h3", "h4", "p", "li", "td", "th", "b",
                        "i", "sup", "sub", "mark"]
 
+    NO_TEXT_ELEMENTS = ["extract", "blockquote", "ul", "ol", "table", "tr"]
+
+    REGEX = re.compile(r"\s+")
+
     @classmethod
     def run(cls, root):
         cls.remove_text_between_blocks(root)
@@ -14,47 +18,30 @@ class Minifier:
 
     @classmethod
     def remove_text_between_blocks(cls, root):
-        for element in root.findall("."):
-            cls.remove_text_element(element)
-        for tag in ["blockquote", "ul", "ol", "table", "tr"]:
-            for element in root.findall(".//" + tag):
-                cls.remove_text_element(element)
-
-    @staticmethod
-    def remove_text_element(element):
-        assert element.text.strip() == ""
-        element.text = None
-        for subelement in element:
-            assert subelement.tail.strip() == ""
-            subelement.tail = None
+        for tag_name in cls.NO_TEXT_ELEMENTS:
+            for element in root.iter(tag_name):
+                element.text = None
+                for subelement in element:
+                    subelement.tail = None
 
     @classmethod
     def strip_all(cls, root):
-        for tag in cls.INLINE_ELEMENTS:
-            for element in root.findall(".//" + tag):
-                cls.strip_element(element)
-
-    @staticmethod
-    def strip_element(element):
-        if element.text:
-            element.text = element.text.lstrip()
-        children = list(element)
-        if len(children) == 0:
-            element.text = element.text.rstrip()
-        elif children[len(children)-1].tail:
-            children[len(children)-1].tail = children[len(children)-1].tail.rstrip()
+        for tag_name in cls.INLINE_ELEMENTS:
+            for element in root.iter(tag_name):
+                if element.text:
+                    element.text = element.text.lstrip()
+                last_child = element.find('*[last()]')
+                if last_child and last_child.tail:
+                    last_child.tail = last_child.tail.rstrip()
+                if last_child is None and element.text:
+                    element.text = element.text.rstrip()
 
     @classmethod
     def merge_whitespace(cls, root):
-        for tag in cls.INLINE_ELEMENTS:
-            for element in root.findall(".//" + tag):
-                cls.merge_whitespace_in_element(element)
-
-    @staticmethod
-    def merge_whitespace_in_element(element):
-        regex = re.compile(r"\s+")
-        if element.text:
-            element.text = regex.sub(" ", element.text)
-        for subelement in element:
-            if subelement.tail:
-                subelement.tail = regex.sub(" ", subelement.tail)
+        for tag_name in cls.INLINE_ELEMENTS:
+            for element in root.iter(tag_name):
+                if element.text:
+                    element.text = cls.REGEX.sub(" ", element.text)
+                for subelement in element:
+                    if subelement.tail:
+                        subelement.tail = cls.REGEX.sub(" ", subelement.tail)
