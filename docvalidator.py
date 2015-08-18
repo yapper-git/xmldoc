810cbd872818d204d22adabae83c5c28c97b196a
yvand <yvan.duron@gmail.com>
2015-08-18 18:37:09 +0200

    Ajout validateur de documents

---
+++ docvalidator.py
@@ -0,0 +1,15 @@
+#!/usr/bin/env python
+
+"""
+Validates document repository (manifest.json and text.xml)
+"""
+
+from argparse import ArgumentParser
+
+from validator import Validator
+
+parser = ArgumentParser(description=__doc__)
+parser.add_argument("input", help="path to document repository")
+args = parser.parse_args()
+
+Validator(args.input).run()
---
+++ validator.py
@@ -0,0 +1,33 @@
+import json
+from lxml import etree
+import os
+
+
+class Validator:
+
+    XML_SCHEMA = "extract.xsd"
+
+    def __init__(self, path):
+        self.path = path
+
+    def run(self):
+        self.manifest_validation()
+        self.text_validation()
+
+    def manifest_validation(self):
+        manifest_path = os.path.join(self.path, 'manifest.json')
+        manifest_dict = json.load(open(manifest_path,'r'))
+        for key, value in manifest_dict.items():
+            assert key in ['lang', 'title', 'subtitle', 'authors']
+
+    def text_validation(self):
+        xsd_path = os.path.join(os.path.dirname(os.path.realpath(__file__)), self.XML_SCHEMA)
+        text_path = os.path.join(self.path, 'text.xml')
+        xsd_tree = etree.parse(xsd_path)
+        xml_tree = etree.parse(text_path)
+        xmlschema = etree.XMLSchema(xsd_tree)
+        xmlschema.assertValid(xml_tree)
+
+        elements = ['b', 'i', 'sup', 'sub', 'mark']
+        for element in elements:
+            assert len(xml_tree.xpath('.//{0}//{0}'.format(element))) == 0
