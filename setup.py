cb9d79c963ac421639392ff130688f4e33bd9f6c
yvand <yvan.duron@gmail.com>
2016-08-24 22:43:32 +0200

    Release version 0.2

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ from distutils.core import setup
 
 setup(
     name='xmldoc',
-    version='0.1',
+    version='0.2',
     py_modules=['xmldoc'],
     install_requires=['jinja2', 'lxml'],
     description='Tools to manage own XML format for documents',
